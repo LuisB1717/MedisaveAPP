f4370ca6db01803c1b0da5bdc5fed46792aa765a
Luis <luisbarboz17@gmail.com>
2021-04-24 20:13:06 -0500

    Cambios de de última hora

--- medisave/lib/main.dart
+++ medisave/lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:medisave/pages/splashpage.dart';
-import 'package:medisave/pages/Welcome.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: SplashPage(
-      goToPage: Welcome(),
-    ),
+    home: SplashPage(),
   ));
 }
--- medisave/lib/pages/splashpage.dart
+++ medisave/lib/pages/splashpage.dart
@@ -2,30 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:medisave/helpers/appcolor.dart';
 
 class SplashPage extends StatelessWidget {
-  final int duration = 0;
-  Widget goToPage;
-
-  SplashPage({this.goToPage});
+  static Route route() {
+    return MaterialPageRoute(builder: (_) => SplashPage());
+  }
 
   @override
   Widget build(BuildContext context) {
-    Future.delayed(Duration(seconds: this.duration), () {
-      Navigator.push(
-          context, MaterialPageRoute(builder: (context) => this.goToPage));
-    });
-
     return Scaffold(
-      backgroundColor: AppColors.BACKGROUND,
       body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                  child: Image(image: AssetImage('assets/imgs/perfil.jpg'))),
-              Padding(padding: EdgeInsets.only(top: 20)),
-              Padding(padding: EdgeInsets.only(top: 20.0)),
-            ]),
-      ),
+          child: Image.asset(
+        'assets/imgs/perfil.jpg',
+        key: const Key('splash_bloc_image'),
+        width: 150,
+      )),
     );
   }
 }
