94fa74c4900be942c6d906371de52a7cb3345989
Luis <luisbarboz17@gmail.com>
2021-05-02 23:01:42 -0500

    ...

--- medisave/lib/pages/strepAdd.dart
+++ medisave/lib/pages/strepAdd.dart
@@ -15,109 +15,207 @@ class Addalarma extends StatefulWidget {
 }
 
 class _AddalarmaState extends State<Addalarma> {
-  int _currentStep = 0;
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: Text('Agregar Alarma'),
-      ),
-      body: Stepper(
-        steps: _mySteps(),
-        currentStep: this._currentStep,
-        onStepTapped: (step) {
-          setState(() {
-            this._currentStep = step;
-          });
-        },
-        onStepContinue: () {
-          setState(() {
-            if (this._currentStep < this._mySteps().length - 1) {
-              this._currentStep = this._currentStep + 1;
-            } else {
-              //Logic to check if everything is completed
-              print('Completed, check fields.');
-            }
-          });
-        },
-        onStepCancel: () {
-          setState(() {
-            if (this._currentStep > 0) {
-              this._currentStep = this._currentStep - 1;
-            } else {
-              this._currentStep = 0;
-            }
-          });
-        },
-        
-       ),    
-    );
-  }
+  int current_step = 0;
 
-  List<Step> _mySteps() {
-    List<Step> _steps = [
-      Step(
-          isActive: _currentStep >= 0,
-          title: Text('Medicamento'),
-          content: Column(
-            children: [
-              SizedBox(height: 50),
-              TextFormField(
-                  decoration: InputDecoration(
-                labelText: 'Nombre del medicamento',
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-              )),
-              SizedBox(height: 30),
-              MiBoton(),
-              SizedBox(height: 30),
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: 'Cantidad por toma',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20))),
-              ),
-              SizedBox(height: 30),
-            ],
-          )),
-      Step(
-          isActive: _currentStep >= 1,
-          title: Text('Recordatorio'),
-          content: Column(
-            children: [
-              SizedBox(height: 30),
-              IntervaloHoras(),
-              SizedBox(height: 15),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Duración',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                ),
-              ),
-              SizedBox(height: 15),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Mensaje',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                ),
-              ),
-              SizedBox(height: 80),
-            ],
-          )),
-      Step(
-        title: Text('Programar'),
+  List<Step> steps = [
+    Step(
+        isActive: true,
+        title: Text('Medicamento'),
         content: Column(
           children: [
-            DatePickerWidget(),
-            TimePickerWidget(),
-                 ],
-        ),
-        isActive: _currentStep >= 2,
-      )
-    ];
-    return _steps;
+            SizedBox(height: 10),
+            TextFormField(
+                decoration: InputDecoration(
+              labelText: 'Nombre del medicamento',
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+            )),
+            SizedBox(height: 20),
+            MiBoton(),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Cantidad por toma',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20))),
+            ),
+          ],
+        )),
+    Step(
+        isActive: true,
+        title: Text('Recordatorio'),
+        content: Column(
+          children: [
+            SizedBox(height: 10),
+            IntervaloHoras(),
+            SizedBox(height: 20),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Duración',
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Mensaje',
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+              ),
+            ),
+          ],
+        )),
+    Step(
+      title: Text('Programar'),
+      content: Column(
+        children: [
+          SizedBox(height: 10),
+          DatePickerWidget(),
+          SizedBox(height: 20),
+          TimePickerWidget(),
+        ],
+      ),
+      isActive: true,
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Agregar Alarma'),
+      ),
+      body: Container(
+        child: Stepper(
+            currentStep: this.current_step,
+            steps: steps,
+            type: StepperType.vertical,
+            onStepTapped: (step) {
+              setState(() {
+                current_step = step;
+              });
+            },
+            onStepContinue: () {
+              setState(() {
+                if (current_step < steps.length - 1) {
+                  current_step = current_step + 1;
+                } else {
+                  current_step = 0;
+                }
+              });
+            },
+            onStepCancel: () {
+              setState(() {
+                if (current_step > 0) {
+                  current_step = current_step - 1;
+                } else {
+                  current_step = 0;
+                }
+              });
+            }),
+      ),
+    );
   }
 }
+
+//       body: Stepper(
+//         steps: _mySteps(),
+//         currentStep: this._currentStep,
+//         onStepTapped: (step) {
+//           setState(() {
+//             this._currentStep = step;
+//           });
+//         },
+//         onStepContinue: () {
+//           setState(() {
+//             if (this._currentStep < this._mySteps().length - 1) {
+//               this._currentStep = this._currentStep + 1;
+//             } else {
+//               //Logic to check if everything is completed
+//               print('Completed, check fields.');
+//             }
+//           });
+//         },
+//         onStepCancel: () {
+//           setState(() {
+//             if (this._currentStep > 0) {
+//               this._currentStep = this._currentStep - 1;
+//             } else {
+//               this._currentStep = 0;
+//             }
+//           });
+//         },
+
+//        ),
+//     );
+//   }
+
+//   List<Step> _mySteps() {
+//     List<Step> _steps = [
+//       Step(
+//           isActive: _currentStep >= 0,
+//           title: Text('Medicamento'),
+//           content: Column(
+//             children: [
+//               SizedBox(height: 50),
+//               TextFormField(
+//                   decoration: InputDecoration(
+//                 labelText: 'Nombre del medicamento',
+//                 border:
+//                     OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+//               )),
+//               SizedBox(height: 30),
+//               MiBoton(),
+//               SizedBox(height: 30),
+//               TextFormField(
+//                 decoration: InputDecoration(
+//                     labelText: 'Cantidad por toma',
+//                     border: OutlineInputBorder(
+//                         borderRadius: BorderRadius.circular(20))),
+//               ),
+//               SizedBox(height: 30),
+//             ],
+//           )),
+//       Step(
+//           isActive: _currentStep >= 1,
+//           title: Text('Recordatorio'),
+//           content: Column(
+//             children: [
+//               SizedBox(height: 30),
+//               IntervaloHoras(),
+//               SizedBox(height: 15),
+//               TextFormField(
+//                 keyboardType: TextInputType.number,
+//                 decoration: InputDecoration(
+//                   labelText: 'Duración',
+//                   border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(20)),
+//                 ),
+//               ),
+//               SizedBox(height: 15),
+//               TextFormField(
+//                 decoration: InputDecoration(
+//                   labelText: 'Mensaje',
+//                   border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(20)),
+//                 ),
+//               ),
+//               SizedBox(height: 80),
+//             ],
+//           )),
+//       Step(
+//         title: Text('Programar'),
+//         content: Column(
+//           children: [
+//             DatePickerWidget(),
+//             TimePickerWidget(),
+//                  ],
+//         ),
+//         isActive: _currentStep >= 2,
+//       )
+//     ];
+//     return _steps;
+//   }
+// }
